a5de1c665013cca9d43a8aab8b44fb2a7c90c636
donglv-0139 <dong.lv184069@sis.hust.edu.vn>
2022-06-04 13:51:22 +0700

    Add continue 1:45 and fix some error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
-import Home from './pages/Home'
-import AddEditBlog from './pages/AddEditBlog';
-import Blog from './pages/Blog';
-import About from './pages/About';
-import NotFound from './pages/AddEditBlog';
-import {ToastContainer} from "react-toastify"
-import "react-toastify/dist/ReactToastify.css"
-import Header from './components/Header';
+import "./App.css";
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import AddEditBlog from "./pages/AddEditBlog";
+import Blog from "./pages/Blog";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Header from "./components/Header";
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
         <Header />
         <ToastContainer />
         <Routes>
-          <Route path="/" element={<Home />}/>
+          <Route path="/" element={<Home />} />
           <Route path="/addBlog" element={<AddEditBlog />} />
           <Route path="/editBlog/:id" element={<AddEditBlog />} />
           <Route path="/blog/:id" element={<Blog />} />
